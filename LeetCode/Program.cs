9ee2551acf940dedb91e80df8cf8b7365c8998a9
Max <Max@MAXVOLOSHIN>
2024-04-07 22:36:12 +0300

    Добавьте файлы проекта.

---
+++ LeetCode/Program.cs
@@ -0,0 +1,49 @@
+﻿using LeetCode.Solutions;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace LeetCode
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+
+            /* Solution_876.ListNode node6 = new Solution_876.ListNode()
+             {
+                 val = 1,
+             };
+
+             Solution_876.ListNode node5 = new Solution_876.ListNode()
+             {
+                 next = node6,
+                 val = 1,
+             };
+
+             Solution_876.ListNode node4 = new Solution_876.ListNode()
+             {
+                 next = node5,
+                 val = 1,
+             };
+
+             Solution_876.ListNode node3 = new Solution_876.ListNode()
+             {
+                 next = node4,
+                 val = 1,
+             };
+
+             Solution_876.ListNode node2 = new Solution_876.ListNode()
+             {
+                 next = node3,
+                 val = 1,
+             };
+
+             Solution_876.ListNode node1 = new Solution_876.ListNode()
+             {
+                 next = node2,
+                 val = 1,
+             };*/
+            Console.WriteLine(Solution_Array_1.Solution_Introduction.FindNumbers([12, 345, 2, 6, 7896]));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LeetCode/Solutions/TestSolution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Solutions
+{
+    public class TestSolution
+    {
+        public static int Sum(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
